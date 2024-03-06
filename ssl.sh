d1983ac729a58663a711205da03a7f546c8ca14b
Lily <153395462+Entrpix@users.noreply.github.com>
2024-03-05 22:18:45 -0500

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -1,4 +1,4 @@
 sudo systemctl reload nginx
 sudo ufw allow 'Nginx Full'
 sudo ufw delete allow 'Nginx HTTP'
-sudo certbot --nginx -d subdomain.domain.tld -d subdomain.domain.tld
+sudo certbot --nginx -d subdomain.domain.tld
